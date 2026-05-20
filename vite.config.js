7f98fdcccc9a949fa5c083a8f086147094e70de4
ruruchin <konstantinzorenko3@gmail.com>
2026-05-20 06:04:14 +0300

    fix: сборка Vite 5 и ESM, исправление exports is not defined на проде

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  build: {
+    target: 'es2020',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
